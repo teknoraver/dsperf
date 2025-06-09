28d5c2e600f4a3a367fae47a15a234b922ab01d2
SebastianoMeduri <sebastiano.meduri@gmail.com>
2025-06-09 16:15:11 +0200

    Added scripts overlay ipv4

---
+++ scripts/test_tool-wifi.sh
@@ -0,0 +1,86 @@
+#!/bin/bash
+# Version: v0.1
+# Copyright 2019,2025 (@) Sebyone Srl
+# Author: Sebastiano Meduri s.meduri@sebyone.it 
+#
+# Scope: network testing using dperf (https://github.com/sebyone/dperf)
+#
+# This Source Code Form is subject to the terms of the Mozilla Public License,v.2.0.
+# You can obtain a copy of the MPL at https://mozilla.org/MPL/2.0/.
+#
+# Disclaimer of Warrant
+# Covered Software is provided under this License on an "as is" basis, without warranty of any kind, either
+# expressed, implied, or statutory, including, without limitation, warranties that the Covered  Software is
+# free of defects, merchantable, fit for a particular purpose or non-infringing.
+# The entire risk as to the quality and performance of the Covered Software is with You.  Should any Covered
+# Software prove defective in any respect, You (not any Contributor) assume the cost of any necessary
+# servicing, repair, or correction.
+# This disclaimer of warranty constitutes an essential part of this License.  
+# No use of any Covered Software is authorized under this License except under this disclaimer.
+
+
+# tool-eth:
+# defines an underlay test using Ipv4 to validate dsperf tool.
+# The model to run is defined in folder: "models\validation_ipv4".
+# The execution environment has "Ethernet" activated and the other interfaces are disabled. 
+
+source models/utils.sh
+
+if [ $# -le 1 ]; then
+  echo "Use: $0 <loopback_addr> <samples> <output_folder_name>"
+  exit 1
+fi
+
+export SCRIPT_BASE=$(pwd)
+
+echo "Base script: $SCRIPT_BASE"
+
+# Level 0 script
+# //
+LOOPBACK_ADDRESS=$1
+SAMPLES=$2
+OUTPUT_NAME=$3
+OUTPUT_FOLDER="results/$3"
+
+
+if [ ! -d "$SCRIPT_BASE/results" ]; then
+  mkdir "$SCRIPT_BASE/results"
+fi
+
+
+if [ ! -d "$OUTPUT_FOLDER" ]; then
+  mkdir $OUTPUT_FOLDER
+else
+  rm -R $OUTPUT_FOLDER
+  mkdir $OUTPUT_FOLDER
+  # deletes all files in folder...
+fi
+
+# Model settings
+# //
+TEST_MODEL_FOLDER="validation-ipv4"
+
+# Environment settings
+# //
+# // disable WiFi
+# // enable eth0
+
+
+# runs model for each cluster we need
+./models/run_test.sh $TEST_MODEL_FOLDER $LOOPBACK_ADDRESS 10k $SAMPLES  "$OUTPUT_FOLDER"
+./models/run_test.sh $TEST_MODEL_FOLDER $LOOPBACK_ADDRESS 100k $SAMPLES  "$OUTPUT_FOLDER"
+./models/run_test.sh $TEST_MODEL_FOLDER $LOOPBACK_ADDRESS 500k $SAMPLES "$OUTPUT_FOLDER"
+./models/run_test.sh $TEST_MODEL_FOLDER $LOOPBACK_ADDRESS 1M $SAMPLES "$OUTPUT_FOLDER"
+./models/run_test.sh $TEST_MODEL_FOLDER $LOOPBACK_ADDRESS 5M $SAMPLES "$OUTPUT_FOLDER"
+./models/run_test.sh $TEST_MODEL_FOLDER $LOOPBACK_ADDRESS 10M $SAMPLES "$OUTPUT_FOLDER"
+
+# Environment reset
+# //
+# // enable WiFi
+# // disable eth0
+
+
+# Merge all csv files located in OUTPUT_FOLDER
+# //
+DATA_FILE=$(date +"%d%B%H%M%S")
+merge_files $OUTPUT_FOLDER "${SCRIPT_BASE}/tool_wlan0_${OUTPUT_NAME}_${SAMPLES}_${DATA_FILE}.csv"
